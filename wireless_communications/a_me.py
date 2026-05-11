5017080a9f5cf97099f81ba16d7c1c8b23e88c70
Parvez Ali <85557535+hereparvezali@users.noreply.github.com>
2026-05-12 00:28:23 +0600

    nothing

--- wireless_communications/a_me.py
+++ wireless_communications/a_me.py
@@ -1,14 +1,34 @@
 import numpy as np
 from commpy import channelcoding
+from matplotlib import pyplot as plt
 
-rng = np.random.default_rng(10)
-n = 2000
+rng = np.random.default_rng(100)
+n = 10000
 data = rng.integers(0, 2, n)
+spread = 8
+pn = rng.choice([-1, 1], spread)
+rate = 1 / 2
 
 conv_trellis = channelcoding.Trellis(np.array([2]), np.array([[0o7, 0o5]]))
 
-for snr in np.arange(5):
-    sigma = np.sqrt(1 / (2 * snr))
-
+bers = []
+for snr_db in np.arange(1, 5.1, 0.5):
+    snr = 10 ** (snr_db / 10)
+    sigma = np.sqrt(1 / (2 * snr * rate / spread))
     encoded = channelcoding.conv_encode(data, conv_trellis, termination="term")
-    print(len(data), len(encoded))
+    chips = np.outer(2 * encoded - 1, pn).flatten()
+
+    received = chips + sigma * rng.standard_normal(chips.shape)
+
+    unchiped = ((received.reshape(-1, len(pn)) @ pn.reshape(-1, 1)) / len(pn)).flatten()
+    decision = (unchiped > 0).astype(int)
+    decoded = channelcoding.viterbi_decode(
+        decision, conv_trellis, decoding_type="hard"
+    )[:n]
+
+    ber = np.not_equal(decoded, data).sum() / len(data)
+    bers.append(ber.item())
+print(bers)
+plt.plot(bers)
+plt.show()
+plt.tight_layout()
